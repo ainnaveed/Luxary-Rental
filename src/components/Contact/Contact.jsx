6e71bddb387645f70081564459111b94f76cf649
Zainnaveed <zainnaveed4307@gmail.com>
2024-08-20 12:30:01 +0500

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+const Contact = () => {
+  return (
+    <>
+      <section id="contact">
+        <h1>CONTACT Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error.</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi aliquid
+          similique voluptas!
+        </p>
+        <div className="container">
+          <img src="/about.jpg" alt="about" />
+          <div className="content">
+            <h3>Let's connect</h3>
+            <div>
+              <p>Phone</p>
+              <span>+92 3224052019</span>
+            </div>
+            <div>
+              <p>Email</p>
+              <span>zainnaveed4307@gmail.com</span>
+            </div>
+            <div>
+              <p>Address</p>
+              <span>Mufti Street Chohan Park IslamPura Lahore</span>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Contact;
